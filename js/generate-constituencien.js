9dc427ccd1180e821b410da45d782b273808fb5f
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-03-18 14:16:39 +0000

    Query twitter attempt 1

--- js/generate-constituencien.js
+++ js/generate-constituencien.js
@@ -2,7 +2,12 @@ var r = require("request");
 var http = require('http');
 var resString ='';
 var resObject;
-
+var dbEndPoint = "localhost:7474/db/data/transaction/commit";
+var username = 'neo4j', password = '12345Jm';
+var url = 'http://' + username + ':' + password + '@' + dbEndPoint;
+var params = {limit: 10}
+var cb = function(err,data) { console.log(JSON.stringify(data)); }
+    
 http.request('http://irish-elections.storyful.com/candidates.json', 
     function(res){
         res.on('data', function(data){
@@ -25,22 +30,32 @@ function cypher(url,query,params,cb) {
 function neoCreate(election){
     // console.log(election);
     console.log(election.candidates[0]);
-    var dbEndPoint = "localhost:7474/db/data/transaction/commit";
-    var username = 'neo4j', password = '12345Jm';
-    var url = 'http://' + username + ':' + password + '@' + dbEndPoint;
-    // var query="MATCH (n:User) RETURN n, labels(n) as l LIMIT {limit}"
+
     
     var statements = [];
 
-    var params = {limit: 10}
-    var cb = function(err,data) { console.log(JSON.stringify(data)); }
-    
     for(var i = 0; i < election.candidates.length; i++){
-        var query2= "CREATE (n:Candidate { first_name : '"+election.candidates[i].first_name+"', last_name : '"+ election.candidates[i].last_name+"'})";
-        cypher(url, query2 ,params,cb);
+        cypher(url, queryBuilder(election.candidates[i]) ,params,cb);
     }
     
 };
 
+function queryBuilder(candidate){
+    var query= "CREATE (n:Candidate { first_name : '" +candidate.first_name+ "', last_name : '" +candidate.last_name+ "', gender : '" +candidate.gender+ "'";
+    
+    if(candidate.website_url !== null){
+        query += ", website : '" +candidate.website_url+ "'"; 
+    }
+    
+    if(candidate.twitter_url !== null){
+        query += ", twitter_url : '" +candidate.twitter_url+ "'"; 
+        var queryTwitter = "CREATE (n:Twitter { url : '" +candidate.twitter_url+"'";
+        query += "}), "+queryTwitter;
+    }
+    
+    
+    query += "})";
+    return query;
+}
 
 
