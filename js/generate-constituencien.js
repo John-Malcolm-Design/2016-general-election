4d23318ca43044749bfc3edfa67b41b71883f9aa
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-03-18 13:15:58 +0000

    Filled database with 500 candidates

--- js/generate-constituencien.js
+++ js/generate-constituencien.js
@@ -16,23 +16,30 @@ http.request('http://irish-elections.storyful.com/candidates.json',
     }
 ).end();
     
-function cypher(url, query,params,cb) {
+function cypher(url,query,params,cb) {
     r.post({uri:url,
-        json:{statements:[{statement:query,parameters:params}]}},
+         json:{statements:[{statement:query}]}},
         function(err,res) { cb(err,res.body)})
 }
 
 function neoCreate(election){
-    console.log(election);
+    // console.log(election);
     console.log(election.candidates[0]);
     var dbEndPoint = "localhost:7474/db/data/transaction/commit";
     var username = 'neo4j', password = '12345Jm';
     var url = 'http://' + username + ':' + password + '@' + dbEndPoint;
-    var query="MATCH (n:User) RETURN n, labels(n) as l LIMIT {limit}"
+    // var query="MATCH (n:User) RETURN n, labels(n) as l LIMIT {limit}"
+    
+    var statements = [];
+
     var params = {limit: 10}
     var cb = function(err,data) { console.log(JSON.stringify(data)); }
-
-    cypher(url, query,params,cb);
+    
+    for(var i = 0; i < election.candidates.length; i++){
+        var query2= "CREATE (n:Candidate { first_name : '"+election.candidates[i].first_name+"', last_name : '"+ election.candidates[i].last_name+"'})";
+        cypher(url, query2 ,params,cb);
+    }
+    
 };
 
 
